b7ad57dc333d0ac78d5e0a3af2d905046d832c88
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-11-07 14:05:53 -0800

    In main.js, add function to fetch the data from mongoDB. #2

--- public/main.js
+++ public/main.js
@@ -8,6 +8,13 @@ const $brookSide = document.querySelector('#brook-side')
 const $jakeSide = document.querySelector('#jake-side')
 const $allefSide = document.querySelector('#allef-side')
 
+const $brookImg = document.querySelector('#brook-img')
+const $jakeImg = document.querySelector('#jake-img')
+const $allefImg = document.querySelector('#allef-img')
+const $brookSideImg = document.querySelector('#brook-img-side')
+const $jakeSideImg = document.querySelector('#jake-img-side')
+const $allefSideImg = document.querySelector('#allef-img-side')
+
 const $brookTitle = document.querySelector('#brook-title')
 const $jakeTitle = document.querySelector('#jake-title')
 const $allefTitle = document.querySelector('#allef-title')
@@ -21,22 +28,42 @@ const $allefSideText = document.querySelector('#allef-text-side')
 
 $meetTheTeam.textContent = 'Meet The Team'
 
-$brook.textContent = 'BROOK CAGLE'
-$brookTitle.textContent = 'designer'
+const getData = () => {
+  Promise.all([
+    fetch('https://techi.envivent.com/description.json')
+      .then(res => res.json())
+      .then(data => {
+        $brookTitle.textContent = data.employees[1].title.toLowerCase()
+        $brookSideTitle.textContent = data.employees[1].title.toLowerCase()
+        $jakeTitle.textContent = data.employees[4].title.toLowerCase()
+        $jakeSideTitle.textContent = data.employees[4].title.toLowerCase()
+        $allefTitle.textContent = data.employees[0].title.toLowerCase()
+        $allefSideTitle.textContent = data.employees[0].title.toLowerCase()
+        $allefText.textContent = data.employees[0].description
+        $allefSideText.textContent = data.employees[0].description
+      }),
+    fetch('https://techi.envivent.com/images.json')
+      .then(res => res.json())
+      .then(data => {
+        const url = 'https://techi.envivent.com/employees/'
+        $brookImg.src = url + data.employees[1].full
+        $jakeImg.src = url + data.employees[4].full
+        $allefImg.src = url + data.employees[0].full
+        $brookSideImg.src = url + data.employees[1].full
+        $jakeSideImg.src = url + data.employees[4].full
+        $allefSideImg.src = url + data.employees[0].full
+      }),
+    fetch('https://techi.envivent.com/names.json')
+      .then(res => res.json())
+      .then(data => {
+        $brook.textContent = data.employees[1].first_name.toUpperCase() + ' ' + data.employees[1].last_name.toUpperCase()
+        $brookSide.textContent = data.employees[1].first_name.toUpperCase() + ' ' + data.employees[1].last_name.toUpperCase()
+        $jake.textContent = data.employees[4].first_name.toUpperCase() + ' ' + data.employees[4].last_name.toUpperCase()
+        $jakeSide.textContent = data.employees[4].first_name.toUpperCase() + ' ' + data.employees[4].last_name.toUpperCase()
+        $allef.textContent = data.employees[0].first_name.toUpperCase() + ' ' + data.employees[0].last_name.toUpperCase()
+        $allefSide.textContent = data.employees[0].first_name.toUpperCase() + ' ' + data.employees[0].last_name.toUpperCase()
+      })
+  ])
+}
 
-$brookSide.textContent = 'BROOK CAGLE'
-$brookSideTitle.textContent = 'designer'
-
-$jake.textContent = 'JAKE YOUNG'
-$jakeTitle.textContent = 'web developer'
-
-$jakeSide.textContent = 'JAKE YOUNG'
-$jakeSideTitle.textContent = 'web developer'
-
-$allef.textContent = 'ALLEF VINICIUS'
-$allefTitle.textContent = 'project manager'
-$allefText.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse mollis aliquam massa, vel vestibulum ipsum tincidunt a. Aliquam nec arcu viverra, consequat neque et, iaculis risus. Etiam ut fringilla orci. Aenean eget est nibh. Integer tempor dui vel semper porttitor. Etiam et ex ornare nibh interdum aliquam quis a eros. Aenean eu scelerisque quam. Etiam eu massa eros. Mauris non quam nec ex porta molestie in non ante. Vivamus ultricies, arcu in tincidunt gravida, orci metus vestibulum dui, nec eleifend ante purus ac tellus. Duis et odio consequat, maximus tellus non, molestie enim.'
-
-$allefSide.textContent = 'ALLEF VINICIUS'
-$allefSideTitle.textContent = 'project manager'
-$allefSideText.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse mollis aliquam massa, vel vestibulum ipsum tincidunt a. Aliquam nec arcu viverra, consequat neque et, iaculis risus. Etiam ut fringilla orci. Aenean eget est nibh. Integer tempor dui vel semper porttitor. Etiam et ex ornare nibh interdum aliquam quis a eros. Aenean eu scelerisque quam. Etiam eu massa eros. Mauris non quam nec ex porta molestie in non ante. Vivamus ultricies, arcu in tincidunt gravida, orci metus vestibulum dui, nec eleifend ante purus ac tellus. Duis et odio consequat, maximus tellus non, molestie enim.'
+getData()
