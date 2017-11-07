51e78c036c1aec682f3fb47a9be305749f1848e6
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-11-07 10:41:44 -0800

    Install necessary npm packages. #1

---
+++ main.js
@@ -0,0 +1,23 @@
+const $meetTheTeam = document.querySelector('#header-text')
+
+const $brook = document.querySelector('#brook')
+const $jake = document.querySelector('#jake')
+const $allef = document.querySelector('#allef')
+
+const $brookTitle = document.querySelector('#brook-title')
+const $jakeTitle = document.querySelector('#jake-title')
+const $allefTitle = document.querySelector('#allef-title')
+
+const $allefText = document.querySelector('#allef-text')
+
+$meetTheTeam.textContent = 'Meet The Team'
+
+$brook.textContent = 'BROOK CAGLE'
+$brookTitle.textContent = 'designer'
+
+$jake.textContent = 'JAKE YOUNG'
+$jakeTitle.textContent = 'web developer'
+
+$allef.textContent = 'ALLEF VINICIUS'
+$allefTitle.textContent = 'project manager'
+$allefText.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse mollis aliquam massa, vel vestibulum ipsum tincidunt a. Aliquam nec arcu viverra, consequat neque et, iaculis risus. Etiam ut fringilla orci. Aenean eget est nibh. Integer tempor dui vel semper porttitor. Etiam et ex ornare nibh interdum aliquam quis a eros. Aenean eu scelerisque quam. Etiam eu massa eros. Mauris non quam nec ex porta molestie in non ante. Vivamus ultricies, arcu in tincidunt gravida, orci metus vestibulum dui, nec eleifend ante purus ac tellus. Duis et odio consequat, maximus tellus non, molestie enim.'
