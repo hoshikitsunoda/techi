bbef3cfa6fac6058983e2c21b99be613f15d2c2c
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-11-07 11:20:56 -0800

    In seed.js, add mongo client to insert text and photo to the server. #1

--- main.js
+++ main.js
@@ -4,20 +4,39 @@ const $brook = document.querySelector('#brook')
 const $jake = document.querySelector('#jake')
 const $allef = document.querySelector('#allef')
 
+const $brookSide = document.querySelector('#brook-side')
+const $jakeSide = document.querySelector('#jake-side')
+const $allefSide = document.querySelector('#allef-side')
+
 const $brookTitle = document.querySelector('#brook-title')
 const $jakeTitle = document.querySelector('#jake-title')
 const $allefTitle = document.querySelector('#allef-title')
 
+const $brookSideTitle = document.querySelector('#brook-title-side')
+const $jakeSideTitle = document.querySelector('#jake-title-side')
+const $allefSideTitle = document.querySelector('#allef-title-side')
+
 const $allefText = document.querySelector('#allef-text')
+const $allefSideText = document.querySelector('#allef-text-side')
 
 $meetTheTeam.textContent = 'Meet The Team'
 
 $brook.textContent = 'BROOK CAGLE'
 $brookTitle.textContent = 'designer'
 
+$brookSide.textContent = 'BROOK CAGLE'
+$brookSideTitle.textContent = 'designer'
+
 $jake.textContent = 'JAKE YOUNG'
 $jakeTitle.textContent = 'web developer'
+
+$jakeSide.textContent = 'JAKE YOUNG'
+$jakeSideTitle.textContent = 'web developer'
 
 $allef.textContent = 'ALLEF VINICIUS'
 $allefTitle.textContent = 'project manager'
 $allefText.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse mollis aliquam massa, vel vestibulum ipsum tincidunt a. Aliquam nec arcu viverra, consequat neque et, iaculis risus. Etiam ut fringilla orci. Aenean eget est nibh. Integer tempor dui vel semper porttitor. Etiam et ex ornare nibh interdum aliquam quis a eros. Aenean eu scelerisque quam. Etiam eu massa eros. Mauris non quam nec ex porta molestie in non ante. Vivamus ultricies, arcu in tincidunt gravida, orci metus vestibulum dui, nec eleifend ante purus ac tellus. Duis et odio consequat, maximus tellus non, molestie enim.'
+
+$allefSide.textContent = 'ALLEF VINICIUS'
+$allefSideTitle.textContent = 'project manager'
+$allefSideText.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse mollis aliquam massa, vel vestibulum ipsum tincidunt a. Aliquam nec arcu viverra, consequat neque et, iaculis risus. Etiam ut fringilla orci. Aenean eget est nibh. Integer tempor dui vel semper porttitor. Etiam et ex ornare nibh interdum aliquam quis a eros. Aenean eu scelerisque quam. Etiam eu massa eros. Mauris non quam nec ex porta molestie in non ante. Vivamus ultricies, arcu in tincidunt gravida, orci metus vestibulum dui, nec eleifend ante purus ac tellus. Duis et odio consequat, maximus tellus non, molestie enim.'
