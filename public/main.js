63e18f053ccdc12435f1ded07a6527a53003574f
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-11-10 16:11:57 -0800

    Added size adjust to the page. #2

--- public/main.js
+++ public/main.js
@@ -28,42 +28,57 @@ const $allefSideText = document.querySelector('#allef-text-side')
 
 $meetTheTeam.textContent = 'Meet The Team'
 
+const employee = {
+  first_name: null,
+  last_name: null,
+  title: null,
+  description: null,
+  image: 'https://techi.envivent.com/employees/' + null
+}
+
 const getData = () => {
   Promise.all([
     fetch('https://techi.envivent.com/description.json')
       .then(res => res.json())
+      .then(result => result.employees)
       .then(data => {
-        $brookTitle.textContent = data.employees[1].title.toLowerCase()
-        $brookSideTitle.textContent = data.employees[1].title.toLowerCase()
-        $jakeTitle.textContent = data.employees[4].title.toLowerCase()
-        $jakeSideTitle.textContent = data.employees[4].title.toLowerCase()
-        $allefTitle.textContent = data.employees[0].title.toLowerCase()
-        $allefSideTitle.textContent = data.employees[0].title.toLowerCase()
-        $allefText.textContent = data.employees[0].description
-        $allefSideText.textContent = data.employees[0].description
+        $brookTitle.textContent = data[1].title.toLowerCase()
+        $brookSideTitle.textContent = data[1].title.toLowerCase()
+        $jakeTitle.textContent = data[4].title.toLowerCase()
+        $jakeSideTitle.textContent = data[4].title.toLowerCase()
+        $allefTitle.textContent = data[0].title.toLowerCase()
+        $allefSideTitle.textContent = data[0].title.toLowerCase()
+        $allefText.textContent = data[0].description
+        $allefSideText.textContent = data[0].description
       }),
     fetch('https://techi.envivent.com/images.json')
       .then(res => res.json())
+      .then(result => result.employees)
       .then(data => {
         const url = 'https://techi.envivent.com/employees/'
-        $brookImg.src = url + data.employees[1].full
-        $jakeImg.src = url + data.employees[4].full
-        $allefImg.src = url + data.employees[0].full
-        $brookSideImg.src = url + data.employees[1].full
-        $jakeSideImg.src = url + data.employees[4].full
-        $allefSideImg.src = url + data.employees[0].full
+        $brookImg.src = url + data[1].full
+        $jakeImg.src = url + data[4].full
+        $allefImg.src = url + data[0].full
+        $brookSideImg.src = url + data[1].full
+        $jakeSideImg.src = url + data[4].full
+        $allefSideImg.src = url + data[0].full
       }),
     fetch('https://techi.envivent.com/names.json')
       .then(res => res.json())
+      .then(result => result.employees)
       .then(data => {
-        $brook.textContent = data.employees[1].first_name.toUpperCase() + ' ' + data.employees[1].last_name.toUpperCase()
-        $brookSide.textContent = data.employees[1].first_name.toUpperCase() + ' ' + data.employees[1].last_name.toUpperCase()
-        $jake.textContent = data.employees[4].first_name.toUpperCase() + ' ' + data.employees[4].last_name.toUpperCase()
-        $jakeSide.textContent = data.employees[4].first_name.toUpperCase() + ' ' + data.employees[4].last_name.toUpperCase()
-        $allef.textContent = data.employees[0].first_name.toUpperCase() + ' ' + data.employees[0].last_name.toUpperCase()
-        $allefSide.textContent = data.employees[0].first_name.toUpperCase() + ' ' + data.employees[0].last_name.toUpperCase()
+        $brook.textContent = data[1].first_name.toUpperCase() + ' ' + data[1].last_name.toUpperCase()
+        $brookSide.textContent = data[1].first_name.toUpperCase() + ' ' + data[1].last_name.toUpperCase()
+        $jake.textContent = data[4].first_name.toUpperCase() + ' ' + data[4].last_name.toUpperCase()
+        $jakeSide.textContent = data[4].first_name.toUpperCase() + ' ' + data[4].last_name.toUpperCase()
+        $allef.textContent = data[0].first_name.toUpperCase() + ' ' + data[0].last_name.toUpperCase()
+        $allefSide.textContent = data[0].first_name.toUpperCase() + ' ' + data[0].last_name.toUpperCase()
       })
   ])
+  // .then(res => {
+  //   const result = [].concat.apply([], res)
+  //   console.log(result);
+  // })
 }
 
 getData()
